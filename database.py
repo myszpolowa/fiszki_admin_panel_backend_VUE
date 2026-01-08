bde95f7b15cf5d8f3f59db0d5389780aa8e30ce9
myszpolowa <mila.sharets607@gmail.com>
2026-01-08 16:40:06 +0100

    change database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 # database.py в fiszki_pycharm (GATEWAY)
+import os
 import httpx
 from fastapi import HTTPException
 
-DB_SERVICE_URL = "http://127.0.0.1:8003"  #  DB Service
+DB_SERVICE_URL = os.getenv("DB_SERVICE_URL", "http://127.0.0.1:8003")
 
 async def get_db() -> httpx.AsyncClient:
-
     client = httpx.AsyncClient(base_url=DB_SERVICE_URL, timeout=10.0)
     try:
         resp = await client.get("/health")
