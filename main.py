c565905575d156e32a4b5362f00f3ff86ffe4932
myszpolowa <mila.sharets607@gmail.com>
2026-01-11 16:47:25 +0100

    Update CORS

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "*",
+        "fiszkiadminpanelfrontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
