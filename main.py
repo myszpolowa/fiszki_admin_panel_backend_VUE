91d8c019061a80da183376d71ea45effba3f98cf
myszpolowa <mila.sharets607@gmail.com>
2026-01-11 17:06:04 +0100

    fix

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ app.add_middleware(
     allow_origins=CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"], )
 
 
 # -------------------------------------------------
